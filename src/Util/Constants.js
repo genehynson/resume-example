8a66b4e8e8423d32a1fac08cfbeb2931a9d3ce4b
Gene Hynson <gene.hynson@boeing.com>
2018-07-22 22:20:49 -0400

    added cfignore, tweaked manifest

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -1,4 +1,7 @@
-export const PROFILE_URL = 'http://localhost:4000/profile';
-export const EXPERIENCE_URL = 'http://localhost:5000/work';
-export const EDUCATION_URL = 'http://localhost:6001/education';
-export const SKILLS_URL = 'http://localhost:7000/skills';
+// export const PROFILE_URL = 'http://localhost:4000/profile';
+// export const EXPERIENCE_URL = 'http://localhost:5000/work';
+// export const EDUCATION_URL = 'http://localhost:6001/education';
+// export const SKILLS_URL = 'http://localhost:7000/skills';
+export const PROFILE_URL = 'https://resume-profile.apps.pcfpre-phx.cloud.boeing.com/profile';
+export const EXPERIENCE_URL = 'https://resume-experience.apps.pcfpre-phx.cloud.boeing.com/work';
+export const EDUCATION_URL = 'https://resume-education.apps.pcfpre-phx.cloud.boeing.com/education';
